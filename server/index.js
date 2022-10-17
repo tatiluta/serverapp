64d40af5567d275785dd8e8ae8d296840bbefc4e
tati <k2luta00@students.oamk.fi>
2022-10-17 14:50:09 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -1,30 +1,76 @@
+
+//const mysql = require('mysql2/promise')
+//const config = require('./config')
+//const db=require('./services/db')
+//const todo =require('./services/todo')
 const express = require('express')
 const cors = require('cors')
-const mysql = require('mysql2/promise')
-const config = require('./config')
-
+const todoRouter=require('./routes/todo')
 const app = express()
+
+
 
 app.use(cors()) //will allow client the calls  
 app.use(express.json()) //allows server to read json 
 app.use(express.urlencoded({ extended: false }))
+app.use('/', todoRouter)
 
 const port = 3001
 
 
+app.use((err,req,res,next)=>{
+
+
+    const statusCode=err.statusCode || 500
+    console.error(err.message, err.stack)
+    res.status(statusCode).json({errror:err.message})
+    return
+})
+
+
+app.listen(port)
+
+/*
 app.get("/", async (req, res) => {
 
 
     try {
-        const connection = await mysql.createConnection(config.db)
-        const [result,] = await connection.execute('select * from task')
+        //const connection = await mysql.createConnection(config.db)
+        //const [result,] = await connection.execute('select * from task')
+        //const result =await db.query('select *from task')
 
+        const result = await todo.getAllTasks()
         if (!result) result = []
+
         res.status(200).json(result)
+
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+
+})
+
+app.post("/new", async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(config.db)
+        const [result,] = await connection.execute('insert into task (description) values (?)', [req.body.description])
+        res.status(200).json({id:result.insertId})
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
 })
 
+app.delete("/delete/:id", async (req, res) => {
 
-app.listen(port)
+
+    try {
+        const connection = await mysql.createConnection(config.db)
+       await connection.execute('delete from task where id = ?', [req.params.id])
+        res.status(200).json({id:req.params.id})
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+*/
+
--- src/App.js
+++ src/App.js
@@ -2,32 +2,77 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
-const URL = 'htp://localhost:3001'
+const URL = 'http://localhost:3001/'
+
+
 
 function App() {
 
   const [tasks, setTasks] = useState([])
-
+  const [task, setTask] = useState('')
 
   useEffect(() => {
     axios.get(URL)
       .then((response) => {
         setTasks(response.data)
+        //console.log(response.data)
       }).catch(error => {
         alert(error.response.data.error)
       })
   }, [])
-  
+
   return (
-    <div>
+    <div style={{ margin: '20px' }}>
       <h3>My Tasks</h3>
+      <form>
+        <label>Add new</label>
+        <input value={task} onChange={e => setTask(e.target.value)} />
+        <button type='button' onClick={save}>Save </button>
+
+      </form>
       <ol>
         {tasks.map(task => (
-          <li key={task.id}>{task.description}</li>
+          <li key={task.id}>{task.description} <a href="#" onClick={() => remove(task.id)}>Delete</a></li>
         ))}
       </ol>
     </div>
   );
+
+
+  function save() {
+
+    const json = JSON.stringify({ description: task })
+    axios.post(URL + 'new', json, {
+
+
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+      .then((response) => {
+
+        const addedObject = JSON.parse(json)
+        addedObject.id = response.data.id
+        setTasks(tasks => [...tasks, addedObject])
+        setTask('')
+      }).catch(error => {
+        //console.log(error)
+        alert(error.response.data.error)
+      })
+  }
+
+  function remove(id) {
+    axios.delete(`${URL}delete/${id}`)
+      .then(() => {
+        const newListWithoutRemoved = tasks.filter((item) => item.id !== id)
+        setTasks(newListWithoutRemoved)
+      }).catch(error => {
+       //console.log(error)
+        alert(error.response.data.error)
+      })
+  }
 }
+
 
 export default App;
